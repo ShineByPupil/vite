9aff11a2730e019edc4d4c4f4a18a8c6e1aa7c44
ShineByPupil <522705046@qq.com>
2024-08-11 15:32:42 +0800

    移动前端文件

--- packages/web/components.d.ts
+++ packages/web/components.d.ts
@@ -8,17 +8,9 @@ export {}
 declare module 'vue' {
   export interface GlobalComponents {
     AButton: typeof import('ant-design-vue/es')['Button']
-    AButtonGroup: typeof import('ant-design-vue/es')['ButtonGroup']
-    AForm: typeof import('ant-design-vue/es')['Form']
-    AFormItem: typeof import('ant-design-vue/es')['FormItem']
     AInput: typeof import('ant-design-vue/es')['Input']
-    AInputPassword: typeof import('ant-design-vue/es')['InputPassword']
     AMenu: typeof import('ant-design-vue/es')['Menu']
-    AModal: typeof import('ant-design-vue/es')['Modal']
-    ASelect: typeof import('ant-design-vue/es')['Select']
-    ASelectOption: typeof import('ant-design-vue/es')['SelectOption']
     Aside: typeof import('./src/components/Aside.vue')['default']
-    ATable: typeof import('ant-design-vue/es')['Table']
     Beian: typeof import('./src/components/beian.vue')['default']
     Header: typeof import('./src/components/Header.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
